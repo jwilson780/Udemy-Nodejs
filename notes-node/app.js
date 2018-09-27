9720236794c8fb68561890d9df72cf4c9de50d14
Jake Wilson <jakewilson780@gmail.com>
2018-09-27 14:20:01 -0400

    notes app p2

--- notes-node/app.js
+++ notes-node/app.js
@@ -7,10 +7,17 @@ const yargs = require("yargs");
 
 const argv = yargs.argv;
 var command = argv._[0];
-console.log("Yargs", argv);
 
 if (command === "add") {
-  notes.addNote(argv.title, argv.body);
+  var note = notes.addNote(argv.title, argv.body);
+  if (note) {
+    console.log("Note added successfully!");
+    console.log("_______________________");
+    console.log(`Title: ${note.title}`);
+    console.log(`Body: ${note.body}`);
+  } else {
+    console.log("An error occoured!");
+  }
 } else if (command === "list") {
   notes.getAll();
 } else if (command === "read") {
--- notes-node/playground/json.js
+++ notes-node/playground/json.js
@@ -1,9 +1,31 @@
-//explore how json works
+// //explore how json works
 
-var obj = {
-  name: "Jake"
+// var obj = {
+//   name: "Jake"
+// };
+
+// var stringObj = JSON.stringify(obj);
+// console.log(typeof stringObj);
+// console.log(stringObj);
+
+// var personString = '{"name":"Jake","age":27}';
+// var person = JSON.parse(personString);
+// console.log(typeof person);
+// console.log(person);
+
+const fs = require("fs"); //core so no npm
+
+var origionalNote = {
+  title: "Some Title",
+  body: "Some Body"
 };
 
-var stringObj = JSON.stringify(obj);
-console.log(typeof stringObj);
-console.log(stringObj);
+var origionalNoteString = JSON.stringify(origionalNote);
+
+fs.writeFileSync("notes.json", origionalNoteString);
+
+var noteString = fs.readFileSync("notes.json");
+
+var note = JSON.parse(noteString);
+console.log(typeof note);
+console.log(note);
