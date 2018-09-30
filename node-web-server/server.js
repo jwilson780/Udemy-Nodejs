fbb9ee7d3196330455a7b3959ffab20208a26c82
Jake Wilson <jakewilson780@gmail.com>
2018-09-30 05:12:44 -0400

    more middleware

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,10 +1,29 @@
 const express = require("express");
 const hbs = require("hbs");
+const fs = require("fs");
 
 var app = express();
 
 hbs.registerPartials(__dirname + "/views/partials");
 app.set("view engine", "hbs");
+
+app.use((req, res, next) => {
+  //use next to tell it when we are done
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  fs.appendFile("server.log", log + "\n", err => {
+    if (err) {
+      console.log("Unable to append to server.log");
+    }
+  });
+  console.log(log);
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render("maintence.hbs");
+// });
+
 app.use(express.static(__dirname + "/public"));
 
 hbs.registerHelper("getCurrentYear", () => {
